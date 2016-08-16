2569e2293f296cc07b3165e71956e7af3790266d
Shia <rise.shia@gmail.com>
2016-08-16 17:49:56 +0900

    Setup root path

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file,
   # see http://guides.rubyonrails.org/routing.html
+
+  root "tasks#index"
 end
+
