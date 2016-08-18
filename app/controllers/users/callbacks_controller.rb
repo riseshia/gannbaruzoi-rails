60778e07f192d9fcdd870a4b272142c18ee4fba2
Shia <rise.shia@gmail.com>
2016-08-18 17:42:36 +0900

    Add remember me when login

--- app/controllers/users/callbacks_controller.rb
+++ app/controllers/users/callbacks_controller.rb
@@ -4,6 +4,7 @@ module Users
   class CallbacksController < Devise::OmniauthCallbacksController
     def github
       @user = User.from_omniauth(request.env["omniauth.auth"])
+      @user.remember_me = true
       sign_in_and_redirect @user
     end
   end
